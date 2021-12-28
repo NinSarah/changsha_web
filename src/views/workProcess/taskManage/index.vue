448b2050853578c654d8b690e0211135b74217ee
xiongyifang <1258388342@qq.com>
2021-12-28 11:31:33 +0800

    init

--- src/views/workProcess/taskManage/index.vue
+++ src/views/workProcess/taskManage/index.vue
@@ -1,38 +1,39 @@
 <template>
   <div class="taskManage_container">
     <el-form :model="queryParams" ref="queryForm" :inline="true" label-width="100px">
-      <el-form-item prop="where.equipmentName" label="仪器名称">
-        <el-input v-model="queryParams.where.equipmentName" placeholder="请输入仪器名称" clearable size="small"
+      <el-form-item prop="where.taskName" label="任务名称">
+        <el-input v-model="queryParams.where.taskName" placeholder="请输入任务名称" clearable size="small"
           style="width: 160px"  />
       </el-form-item> 
-      <el-form-item prop="where.equipmentUser" label="仪器使用人">
-        <el-input v-model="queryParams.where.equipmentUser" placeholder="请输入仪器使用人" clearable size="small"
-          style="width: 160px"  />
-      </el-form-item>
-      <el-form-item prop="where.deviceStatus" label="仪器状态">
-        <el-select v-model="queryParams.where.deviceStatus" placeholder="请选择" clearable>
+      <el-form-item prop="where.taskStatus" label="任务状态">
+        <el-select v-model="queryParams.where.taskStatus" placeholder="请选择" clearable>
           <el-option
-            v-for="item in equipmentList"
+            v-for="item in taskStatus"
             :key="item.dictCode"
             :label="item.dictLabel"
             :value="item.dictCode">
           </el-option>
         </el-select>
       </el-form-item>
-      <el-form-item  prop="where.createTime" label="仪器使用时间">
-        <el-date-picker v-model="queryParams.where.startTime" value-format="yyyy-MM-dd HH:mm:ss" type="date"
-          placeholder="执行时间" style="width: 200px" />
-        <div style="
-          font-size: 14px;
-          color: #666;
-          line-height: 40px;
-          margin: -2px 6px;
-          display: inline-block;
-        ">
-          至
-        </div>
-        <el-date-picker v-model="queryParams.where.endTime" value-format="yyyy-MM-dd HH:mm:ss" type="date"
-          placeholder="执行结束时间" style="width: 200px" />
+      <el-form-item prop="where.type" label="类型">
+        <el-select v-model="queryParams.where.type" placeholder="请选择" clearable>
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item  prop="where.createTime" label="处置方式">
+        <el-select v-model="queryParams.where.type" placeholder="请选择" clearable>
+          <el-option
+            v-for="item in handleWay"
+            :key="item.dictCode"
+            :label="item.dictLabel"
+            :value="item.dictCode">
+          </el-option>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="el-icon-search" round @click="handleQuery" size="small">筛选</el-button>
@@ -62,18 +63,34 @@ export default {
         page: 1,
         pageSize: 10,
         where: {
-          equipmentName: '',
-          deviceStatus: ''
+          taskName: '',
+          taskStatus: ''
         }
       },
       // 总条数
       total: 0,
+      options: [
+        {
+          value: '01',
+          label: '原材料'
+        },
+        {
+          value: '02',
+          label: '混凝土'
+        }
+      ]
     }
   },
   computed: {
+    taskStatus() {
+      return this.$store.getters.taskStatus
+    },
     // 获取设备状态
     equipmentList() {
       return this.$store.getters.equipmentStatus
+    },
+    handleWay() {
+      return this.$store.getters.handleWays
     },
   },
   mounted() {
